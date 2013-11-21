14ceb802517a7362592de129b62f1875cd51729a
meha <shortckt0250@gmail.com>
2013-11-20 23:00:03 -0500

    Basic Layer2Frame functionalities implemented.

--- src/networksim/Layer3Frame.java
+++ src/networksim/Layer3Frame.java
@@ -2,4 +2,9 @@ package networksim;
 
 public class Layer3Frame  implements FrameInterface{
 
+    @Override
+    public byte[] toByteArray() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
