957bcc72cddbfdae05771b4386b1747999fb08b0
Ken <topgun122104@yahoo.com>
2013-11-26 09:26:05 -0500

    Updated Layer 3

--- src/networksim/Layer3Interface.java
+++ src/networksim/Layer3Interface.java
@@ -1,11 +1,11 @@
 package networksim;
 
 public interface Layer3Interface {
-    public void receiveFromLayer2(Layer2 frame);
+    public void receiveFromLayer2(Layer3 frame);
 
     public void sendToLayer4();
 
-    public void receiveFromLayer4(Layer3 frame);
+    public void receiveFromLayer4(Layer3 frame, String finalDestination);
 
     public void sendToLayer2();
 }
